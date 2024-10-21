48ab113a56f6cdef485bdec3565ffb7dcdb777bb
RajatSharma2004 <rajatsharmayo4@gmail.com>
2024-10-21 14:21:27 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("TEST")
+// Import express
+const express = require('express');
+
+
+const app = express();
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});

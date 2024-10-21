1dfb18c27ff6d8c2e7eee20ee550d9e7d05258ad
RajatSharma2004 <rajatsharmayo4@gmail.com>
2024-10-21 14:31:57 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 // Import express
 const express = require('express');
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
 
-
+connectDb();
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(cors());
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
